f750fd89fadeca9af8ecc43dc0dce7f69b3e37c5
BrianReisman <brianreisman@protonmail.com>
2021-01-27 22:55:03 -0500

    dependencies added for project to package.json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import SideBar from "./components/SideBar";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hi from App.js</h1>
+      <p>I'm a p tag!</p>
+      <SideBar />
+      <hr />
+
+
+      <Route exact path="/">
+        <SideBar />
+      </Route>
+
+      <Route path="/now" component={SideBar} />
+      <Route 
+        path="/work"
+        render={(props) => {
+          console.log(props)
+          return <code>Code!</code>;
+        }}
+        />
     </div>
   );
-}
+};
 
 export default App;
+
+//* <Route> with render={} requires a return
