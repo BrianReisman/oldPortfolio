53aa6b4feda2ee383290024d6c9009773cb2822d
BrianReisman <brianreisman@protonmail.com>
2021-01-28 10:23:22 -0500

    addressed warnings and linting errors. Attemptin to redeploy.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //
 import React from "react";
-import { Route } from "react-router-dom";
+// import { Route } from "react-router-dom";
 import './app.css'
 //components
 import StyledApp from './StyledApp';
